95c7d0d92db40d1314dfba774f810e66e7d5a0fa
jomariepolanco <jomariepolanco@gmail.com>
2020-10-28 11:48:05 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "a")
+Artist.create(name: "b")
+Artist.create(name: "c")
+Artist.create(name: "d")
+Artist.create(name: "e")
+
+Song.create(title: "f", artist_id: 1)
+Song.create(title: "g", artist_id: 2)
+Song.create(title: "h", artist_id: 3)
+Song.create(title: "i", artist_id: 4)
+Song.create(title: "j", artist_id: 5)
